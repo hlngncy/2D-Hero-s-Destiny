2e805268ab626a49b4dbc645a634a5539645cb65
Helen <hlngncy@gmail.com>
2023-06-21 17:46:16 +0300

    Health potions animated

--- Hero's Destiny/Assets/Scripts/Entities/Potions/HealthPotion.cs
+++ Hero's Destiny/Assets/Scripts/Entities/Potions/HealthPotion.cs
@@ -1,11 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class HealthPotion : MonoBehaviour
 {
     [SerializeField] private int _health;
+
+    private void Start()
+    {
+        this.transform.DOMoveY(transform.position.y + 2f, .5f).SetLoops(-1, LoopType.Yoyo);
+    }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
