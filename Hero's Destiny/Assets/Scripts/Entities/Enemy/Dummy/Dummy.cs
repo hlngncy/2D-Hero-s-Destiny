7ba859ce06cfbe73616e577cab52a47d55afefb6
Helen <hlngncy@gmail.com>
2023-06-25 11:15:37 +0300

    tutorial map and enemies done

---
+++ Hero's Destiny/Assets/Scripts/Entities/Enemy/Dummy/Dummy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy : EnemyController
+{
+    protected override void Update()
+    {
+        //DO NOTHING
+    }
+}
